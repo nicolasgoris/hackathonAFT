ad3ef86ad4e86434e374924a1d216f5d1e5d783e
nvanheertum <n.vanheertum@gmail.com>
2015-04-26 01:10:44 +0200

    Get news out database
    News uit database halen ipv vaste array

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,42 +1,24 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, DataBase) {
 	$scope.services = {"computers":true, "laptops":true, "videocameras":false, "studyplaces":true};
 	$scope.today = new Date();
-	$scope.news = [
-		 {
-           "startdate": "2015-04-23",
-           "enddate": "2015-04-24",
-           "title": "wie maakte de laptops kapot?",
-           "text": "lala",
-           "img": ""
-       },
-       {
-           "startdate": "2015-04-24",
-           "enddate": "2015-04-25",
-           "title": "nieuwe goed APp",
-           "text": "de ontwikkeling van de nieuw APp is bijna klaar.",
-           "img": ""
-       },
-       {
-           "startdate": "2015-04-24",
-           "enddate": "2015-04-26",
-           "title": "wie maakte de laptops kapot?",
-           "text": "we hebben de laptops vorig weekend eens nagekeken en hebben vast gesteld dat er ongeveer een 5 tal laptops kapot zijn. We hebben nagetrokken wie dit zouden zijn maar we willen deze personen graag de kans geven om hier zelf voor uit te komen.",
-           "img": ""
-       }
-   ];
-	var news = [];
-	$.each($scope.news, function(key, value) {
-		var startdate = new Date(value.startdate);
-		var enddate = new Date(value.enddate);
-		enddate.setDate(enddate.getDate()+1);
-		if ($scope.today > startdate && $scope.today < enddate) {
-			news.push(value);
-		}
-	});
-	$scope.news = news;
-})
+  var news = [];
+  $.getJSON(DataBase.getDBContent('news'), function(data) {
+    $scope.$apply(function() {
+      $scope.news  = data.rows[0].value;
+      $.each($scope.news, function(key, value) {
+        var startdate = new Date(value.startdate);
+        var enddate = new Date(value.enddate);
+        enddate.setDate(enddate.getDate()+1);
+        if ($scope.today > startdate && $scope.today < enddate) {
+          news.push(value);
+        }
+      });
+      $scope.news = news;
+      });
+    });
+	})
 
 .controller('ReservationCtrl', function($scope, $http, DataBase) {
 	/*$http.get(DataBase.getDBContent('devices'))
